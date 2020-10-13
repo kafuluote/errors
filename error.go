4719d0ba9a6f4fcdd5cbc3c480ddfce895a3b91c
kafu <616194517@qq.com>
2020-10-13 15:29:24 +0800

    init

---
+++ error.go
@@ -0,0 +1,40 @@
+package errors
+
+import (
+	//. "digicon/proto/common"
+	goErrors "errors"
+	"os"
+)
+
+// 兼容golang errors对象
+func New(text string) error {
+	return goErrors.New(text)
+}
+
+func GetErrStatus(err interface{}) int {
+	switch v := err.(type) {
+	case SysErrorInterface:
+		return v.Status()
+	case NormalErrorInterface:
+		return v.Status()
+	default:
+		return 3
+	}
+}
+
+func GetErrMsg(err interface{}) string {
+	switch v := err.(type) {
+	case SysErrorInterface:
+		if os.Getenv("API_ENV") == "prod" { //生产环境只显示概略错误信息
+			return v.Error()
+		} else {
+			return v.String()
+		}
+	case error:
+		return v.Error()
+	case NormalErrorInterface:
+		return v.Error()
+	default:
+		return ""
+	}
+}
